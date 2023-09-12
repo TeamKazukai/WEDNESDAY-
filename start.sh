52a88b3ca258b3e3016a460bdbb491cd93b4740f
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-09-12 16:13:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamKazukai/WEDNESDAY-.git /WEDNESDAY 
+  git clone https://github.com/TeamKazukai/WEDNESDAY.git /WEDNESDAY 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /WEDNESDAY 
